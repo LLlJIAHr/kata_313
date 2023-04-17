6e58675f6e2916f20228c2ca7673090d8d3a0e73
LLIJIAHr <typrostosyn@gmail.com>
2023-04-17 17:56:35 +0500

    kata315 fix

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -3,9 +3,8 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.model.User;
-import ru.kata.spring.boot_security.demo.service.RoleService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.security.Principal;
@@ -15,37 +14,14 @@ import java.security.Principal;
 @RequestMapping("/api")
 public class AdminController {
     private final UserService userService;
-//    private final RoleService roleService;
-//
+
     public AdminController(UserService userService) {
-//        this.roleService = roleService;
         this.userService = userService;
     }
+
     @GetMapping("/admin")
     public String showAllUsers(Model model, Principal principal) {
-//        model.addAttribute("users", userService.getAllUsers());
-//        model.addAttribute("newUser", new User());
-//        model.addAttribute("allRoles", roleService.findAll());
         model.addAttribute("current", userService.findByEmail(principal.getName()));
         return "admin-page";
     }
-
-//    @PostMapping("/adding")
-////    @ModelAttribute("user")
-//    public String createUser(User user) {
-//        userService.addUser(user);
-//        return "redirect:/admin";
-//    }
-//    @PatchMapping("/updating")
-//    public String updateUser(User user) {
-//        userService.updateUser(user);
-//        return "redirect:/admin";
-//    }
-//    @GetMapping("/deleting/{id}")
-//    public String deleteUser(@PathVariable Long id) { //
-//        userService.deleteUser(id);
-//        return "redirect:/admin";
-//    }
-
-
 }
